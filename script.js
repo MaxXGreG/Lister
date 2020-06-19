cf4c96a2e316edf2434177d20cbf3223e30ca6b0
Max G <Maks@MaxBook-Air.local>
2020-06-19 16:29:09 +0200

    Add item function is implemented

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function askName() {
     document.querySelector('#title').insertAdjacentHTML('afterbegin', name + "'s ")
 }
 
+function addItem() {
+    let itemName = document.querySelector("#item").value
+    let item = '<div id="item"><h1 id="item-name">'+ itemName +'</h1> <i class="fas fa-check-square"></i> <i class="fas fa-times"></i></div>'
+    document.querySelector("#list").insertAdjacentHTML("afterbegin", item)
+    document.querySelector("#item").value = ' '
+}
